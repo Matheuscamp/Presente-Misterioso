7aafbe422a3d1878cfea0d168ba5b9b77077d113
Matheuscamp <matheus.camp32@gmail.com>
2023-05-17 10:08:56 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+function sim() {
+  alert("Você aceitou namorar comigo! :)");
+  // redireciona para um URL apos aceitar
+  location.href = "https://www.youtube.com/watch?v=izGwDsrQ1eQ";
+}
+
+function desvia(t) {
+  var btn = t;
+  btn.style.position = "absolute";
+  btn.style.bottom = geraPosicao(10, 90);
+  btn.style.left = geraPosicao(10, 90);
+  console.log("opa, desviei...");
+}
+
+function geraPosicao(min, max) {
+  return Math.random() * (max - min) + min + "%";
+}
